b1437d5457aeb3e465576ccc94d7edf7e3b03faa
teiyou <zhengyang_heu@163.com>
2018-07-20 22:47:36 +0800

    Twocommit

---
+++ DeCorate/DeCorate/Component.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeCorate
+{
+    public abstract class Component
+    {
+        public abstract string Show();
+    }
+
+    public abstract  class DecorateComponent : Component
+    {
+        private Component _component;
+        public DecorateComponent(Component component)
+        {
+            _component = component;
+        }
+
+        public override string Show()
+        {
+           return   _component.Show();
+        }
+    }
+
+
+    public sealed class SafeDecote:DecorateComponent
+    {
+        public SafeDecote(Component component):base(component)
+        {
+
+        }
+        public override string Show()
+        {
+            return base.Show()+"添加安全机制";
+        }
+    }
+
+    public sealed class TransformDecore:DecorateComponent
+    {
+        public TransformDecore(Component component):base(component)
+        {
+
+        }
+        public override string Show()
+        {
+            return base.Show()+"添加平移功能";
+        }
+    }
+
+    public sealed class  CabinDecorate:DecorateComponent
+    {
+        public CabinDecorate(Component component):base(component)
+        {
+
+        }
+
+        public override string Show()
+        {
+            return base.Show()+"厨房修饰";
+        }
+    }
+
+    public class MYHouse : Component
+    {
+        public override string Show()
+        {
+            return "myhouse";
+        }
+    }
+
+}
---
+++ DeCorate/DeCorate/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeCorate
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Component myHouse;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myHouse = new MYHouse();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             myHouse = new SafeDecote(myHouse);
+            this.txt_house.Text= myHouse.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myHouse = new TransformDecore(myHouse);
+            this.txt_house.Text = myHouse.Show();
+        }
+    }
+}
